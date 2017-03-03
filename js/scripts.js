a7490e6447f09c1b284efb9a0d0dc101ebf058f6
ccheyshook <ccheyanneshook@outlook.com>
2017-03-03 13:06:33 -0800

    Fixed Bugs
    * Got layout started
    * Randomized bubble colors
    * Fixed waves

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-// Store 3 images
-var img0 = '../img/200.jpg';
-var img1 = '../img/300.jpg';
-var img2 = '../img/400.jpg';
-
-// Get random number
-var max = 3;
-var numRand = Math.floor( Math.random() * max );
-console.log(numRand);
-
-// Display randomized images
-$('#random').attr( 'src', eval('img' + numRand) );
-
 var google = $('svg').drawsvg({
   duration : 5000,
   reverse: true,
@@ -18,3 +5,21 @@ var google = $('svg').drawsvg({
 });
 
 google.drawsvg('animate');
+
+
+// Get random number
+var max = 5;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+// Store Colors
+var color0 = 'red';
+var color1 = 'yellow';
+var color2 = 'green';
+var color3 = 'purple';
+var color4 = 'orange';
+
+// On page load
+// Set random color
+$('.bubble').css('background-color', eval('color' + numRand));
+
